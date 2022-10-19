866fe3efc20a984f02fe392405e1f484e6082a83
UncleDea <58232532+UncleDea@users.noreply.github.com>
2022-10-18 21:18:40 -0500

    Revert "bug fix as per @demo-123"

--- JavaWebApp/src/main/java/com/example/demo/DemoApplication.java
+++ JavaWebApp/src/main/java/com/example/demo/DemoApplication.java
@@ -9,13 +9,9 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 
-		String name = "vamsi";
+		String name = "Denis"
 
 		System.out.println(name);
-
-		Integer age = "20";
-		System.out.println(age + 5);
-
 	}
 
 }
